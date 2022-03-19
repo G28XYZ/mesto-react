074645c62ed5dc828c25b971b9075ae7e4687937
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-03-19 23:27:51 +0700

    feat: сверстан header в состоянии авторизованного пользователя

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,28 @@ function Header({ loggedIn, email }) {
   const path = location.pathname;
 
   const links = {
-    "/sign-up": <Link to="/sign-in">Вход</Link>,
-    "/sign-in": <Link to="/sign-up">Регистрация</Link>,
-    "/": <Link to="/sign-in">Выйти</Link>,
+    "/sign-up": (
+      <Link to="/sign-in" className="header__link">
+        Вход
+      </Link>
+    ),
+    "/sign-in": (
+      <Link to="/sign-up" className="header__link">
+        Регистрация
+      </Link>
+    ),
+    "/": (
+      <Link to="/sign-in" className="header__link header__link_blackout">
+        Выйти
+      </Link>
+    ),
   };
 
   return (
     <header className="header">
       <img src={logo} alt="Логотип - место" className="header__logo" />
-      <div>
-        {loggedIn && email}
+      <div className="header__auth">
+        <p className="header__email">{loggedIn && email}</p>
         {links[path]}
       </div>
     </header>
